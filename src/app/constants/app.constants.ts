71ca5b2f76a109c8bc142120efb94a3c4f7d3bfa
redabyadi <redabiyadi1999@gmail.com>
2025-11-23 15:52:32 +0100

    JWT

--- src/app/constants/app.constants.ts
+++ src/app/constants/app.constants.ts
@@ -1,7 +1,8 @@
 
 export const  AppConstants = {
     CONTACT_API_URL : "/contact",
-    LOGIN_API_URL : "/user",
+    USER_API_URL : "/user",
+    LOGIN_API_URL : "/apiLogin",
     ACCOUNT_API_URL : "/myAccount",
     BALANCE_API_URL : "/myBalance",
     LOANS_API_URL : "/myLoans",
